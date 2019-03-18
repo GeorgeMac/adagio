25019bea0558704678b4bbc4279435cfeaf08a42
George MacRorie <me@georgemac.com>
2019-03-18 16:19:42 +0000

    Remove hand baked http API in favor of twirp service

--- pkg/api/doc.go
+++
@@ -1,6 +0,0 @@
-// package api
-//
-// This package contains the HTTP API components used by adagio serve
-// It exposes the control plane for adagio used to execute work
-// and report on status of existing work within the system
-package api
