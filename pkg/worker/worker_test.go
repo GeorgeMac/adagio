04c840534eba62f64a4fbd147c1ba01c8ff3f90b
George MacRorie <me@georgemac.com>
2019-06-21 13:50:55 +0100

    Add failing test to ensure claims are not made for non-existant runtime

--- pkg/worker/worker_test.go
+++ pkg/worker/worker_test.go
@@ -79,3 +79,68 @@ func TestPool_HappyPath(t *testing.T) {
 	// ensure runtime was invoked once
 	assert.Equal(t, uint64(1), runtimeCalls)
 }
+
+func TestPool_Error_RuntimeDoesNotExist(t *testing.T) {
+	var (
+		node = &adagio.Node{
+			Spec: &adagio.Node_Spec{
+				Name:    "foo",
+				Runtime: "unknown",
+			},
+		}
+
+		runtimeCalls uint64
+
+		runtimes = map[string]Runtime{
+			"known": RuntimeFunc(func(n *adagio.Node) (*adagio.Result, error) {
+				atomic.AddUint64(&runtimeCalls, 1)
+
+				return &adagio.Result{
+					Conclusion: adagio.Conclusion_ERROR,
+				}, nil
+			}),
+		}
+
+		// new repository which expects 5 subscriptions
+		repo = newRepository(5, node)
+
+		pool = NewPool(&repo, runtimes, WorkerCount(5))
+
+		done         = make(chan struct{})
+		ctxt, cancel = context.WithCancel(context.Background())
+	)
+
+	go func() {
+		pool.Run(ctxt)
+		done <- struct{}{}
+	}()
+
+	// wait for all subscriptions
+	repo.subscriptionCount.Wait()
+
+	// ensure we have five repo calls
+	require.Len(t, repo.subscribeCalls, 5)
+
+	// ensure each subscriber supplied a channel for ready events
+	for _, call := range repo.subscribeCalls {
+		require.NotNil(t, call.events)
+
+		assert.Equal(t, []adagio.Node_Status{adagio.Node_READY}, call.statuses)
+
+		// feed each subscriber an event for node "foo"
+		call.events <- &adagio.Event{RunID: "bar", NodeSpec: &adagio.Node_Spec{Name: "foo", Runtime: "test"}}
+	}
+
+	// stop running
+	cancel()
+	<-done
+
+	// ensure no claims were attempted
+	require.Nil(t, repo.claimCalls)
+
+	// ensure no finish calls were made
+	require.Nil(t, repo.finishCalls)
+
+	// ensure runtime was never invoked
+	assert.Equal(t, uint64(0), runtimeCalls)
+}
