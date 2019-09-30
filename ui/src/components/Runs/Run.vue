e33985ba8fb168fc8abfa42ea85d2390bbcf6880
George MacRorie <me@georgemac.com>
2019-09-30 16:52:20 +0100

    Add run status to Run.vue

--- ui/src/components/Runs/Run.vue
+++ ui/src/components/Runs/Run.vue
@@ -1,16 +1,21 @@
 <template>
   <section class="section">
-    <section class="hero">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title">
-            Run {{ run.id }}
-          </h1>
+    <div class="columns is-centered">
+      <div class="column">
+        <figure class="image">
+          <Graph v-bind:run="run" />
+        </figure>
+      </div>
+      <div class="column has-text-left">
+        <p class="title">run details</p>
+        <p class="subtitle">{{ run.id }}</p>
+        <div class="content">
+          <span :class="status.class">
+            {{ run.status.toLowerCase() }}
+            <b-icon pack="fas" :icon="status.icon" :custom-class="status.custom"></b-icon>
+          </span>
         </div>
       </div>
-    </section>
-    <div class="container">
-      <Graph v-bind:run="run" />
     </div>
   </section>
 </template>
@@ -27,6 +32,32 @@ export default {
   computed: {
     id() {
       return this.$route.params.id
+    },
+    statusText() {
+      var status = this.run.status.toLowerCase();
+      return `${status}`;
+    },
+    status() {
+      switch (this.run.status) {
+        case "RUNNING":
+          return {
+            icon: "spinner",
+            custom: "fa-spin",
+            class: "tag is-warning"
+          }
+        case "COMPLETED":
+          return {
+            icon: "check",
+            custom: "",
+            class: "tag is-success"
+          }
+        default:
+          return {
+            icon: "spinner",
+            custom: "",
+            class: "tag is-light"
+          }
+      }
     }
   },
   data() {
@@ -61,3 +92,9 @@ export default {
   }
 }
 </script>
+
+<style>
+.tag .icon {
+  padding-left: 0.5rem;
+}
+</style>
--- ui/src/main.js
+++ ui/src/main.js
@@ -7,10 +7,10 @@ import router from './router';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 // internal icons
-import { faCheck, faPlus, faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { faCheck, faPlus, faArrowUp, faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faCheck, faPlus, faArrowUp);
+library.add(faCheck, faPlus, faArrowUp, faSpinner);
 Vue.component('vue-fontawesome', FontAwesomeIcon);
 
 Vue.config.productionTip = false
